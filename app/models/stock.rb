3cf26b682eace8a303b40537c927b368ebf6309c
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-01-27 22:11:54 +0300

    UPDATE GITIGNORE & IEX API KEY

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,12 @@
 class Stock < ApplicationRecord
+
+    def self.new_lookup(ticker_symbol)
+        client = IEX::Api::Client.new(
+            publishable_token: Rails.application.credentials.iex_client[:publish_key],
+            secret_token: Rails.application.credentials.iex_client[:secret_key],
+            endpoint: 'https://cloud.iexapis.com/v1'
+          )
+
+        client.quote(ticker_symbol).latest_price
+    end
 end
