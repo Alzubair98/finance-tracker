81366ca3f1188e38cca9292a8eb0b22b8e182ae5
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-01-29 18:22:09 +0300

    fix small letters bug

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,7 +1,7 @@
 class StocksController < ApplicationController
     
     def search
-        @stock = Stock.new_lookup(params[:stock])
+        @stock = Stock.new_lookup(params[:stock].capitalize)
         render 'users/my_portfolio'
     end
   end
