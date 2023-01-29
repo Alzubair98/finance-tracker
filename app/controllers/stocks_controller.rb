c59f9d4c11b415c62c5c80c3f2e48f0e6eacd4f5
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-01-29 14:16:42 +0300

    add details card

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,6 +2,6 @@ class StocksController < ApplicationController
     
     def search
         @stock = Stock.new_lookup(params[:stock])
-        render json: @stock
+        render 'users/my_portfolio'
     end
   end
