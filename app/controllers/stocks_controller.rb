0d71791e605b5af97abe425242c55e607ce4859d
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-01-29 18:25:00 +0300

    fix empty search

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,7 +1,14 @@
 class StocksController < ApplicationController
     
     def search
-        @stock = Stock.new_lookup(params[:stock].capitalize)
-        render 'users/my_portfolio'
+        if params[:stock].present?
+            @stock = Stock.new_lookup(params[:stock].capitalize)
+            render 'users/my_portfolio'
+        else 
+            flash[:alert] = 'Please enter a symbol to search'
+            redirect_to my_portfolio_path
+        end
     end
+
+
   end
