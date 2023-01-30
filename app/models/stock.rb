73990620d7f9e8af85f7fed0f03d48f9793ec894
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-01-30 20:37:12 +0300

    fix stock model

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,8 @@
 class Stock < ApplicationRecord
 
+    has_many :user_stocks
+    has_many :users, through: :user_stocks
+    
     def self.new_lookup(ticker_symbol)
         client = IEX::Api::Client.new(
             publishable_token: Rails.application.credentials.iex_client[:publish_key],
