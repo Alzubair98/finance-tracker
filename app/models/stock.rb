2616134b1412e7a8d55b12dcd039cc99407bd01f
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-01-30 20:44:43 +0300

    migrate files

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,8 +3,8 @@ class Stock < ApplicationRecord
     has_many :user_stocks
     has_many :users, through: :user_stocks
 
+    validates :name, :ticker, presence: true
 
-    validates :name, :ticker_symbol, presence: true
     def self.new_lookup(ticker_symbol)
         client = IEX::Api::Client.new(
             publishable_token: Rails.application.credentials.iex_client[:publish_key],
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_27_182506) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_30_172506) do
   create_table "stocks", force: :cascade do |t|
     t.string "ticker"
     t.string "name"
     t.decimal "last_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "user_stocks", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "stock_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["stock_id"], name: "index_user_stocks_on_stock_id"
+    t.index ["user_id"], name: "index_user_stocks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,4 +40,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_27_182506) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "user_stocks", "stocks"
+  add_foreign_key "user_stocks", "users"
 end
